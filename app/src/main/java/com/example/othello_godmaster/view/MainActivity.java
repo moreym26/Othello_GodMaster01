208f60d1b48b74e3bd2e0709513e02fc4ea5e1cd
malory <moreym26@up.edu>
2023-04-25 20:17:37 -0700

    Test01

---
+++ app/src/main/java/com/example/othello_godmaster/view/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.othello_godmaster.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.othello_godmaster.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
